6f021586f1f0820c7ccef2ae7733fbebdd94b517
AnyLi <616939152@qq.com>
2021-05-27 14:32:47 +0800

    格式化

--- App.vue
+++ App.vue
@@ -1,22 +1,19 @@
 <script>
 	export default {
 		globalData: {
-					baseUrl: '服务器接口地址',
-					staticUrl: '服务器图片地址',
-					token: '',
-					userInfo: null,
-				},
-		onLaunch: function() {
+			baseUrl: '服务器接口地址',
+			staticUrl: '服务器图片地址',
+			token: '',
+			userInfo: null,
 		},
-		onShow: function() {
-		},
-		onHide: function() {
-		}
+		onLaunch: function() {},
+		onShow: function() {},
+		onHide: function() {}
 	}
 </script>
 
 <style>
 	/*每个页面公共css */
 	@import "./utils/styles/base.css";
-		@import "./utils/styles/app.css";
+	@import "./utils/styles/app.css";
 </style>
